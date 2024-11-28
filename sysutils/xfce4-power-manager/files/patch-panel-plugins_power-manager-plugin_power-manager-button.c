ed1d092a07cd110d2c1ecd038ad9bc41a42fbded
Frank Hilgendorf <frank.hilgendorf@posteo.de>
2024-11-28 19:19:43 +0100

    xfce4-power-manager: button.c - changed key binding

--- sysutils/xfce4-power-manager/files/patch-panel-plugins_power-manager-plugin_power-manager-button.c
+++ sysutils/xfce4-power-manager/files/patch-panel-plugins_power-manager-plugin_power-manager-button.c
@@ -1,10 +1,10 @@
---- panel-plugins/power-manager-plugin/power-manager-button.c.orig	2019-05-17 13:43:56 UTC
+--- panel-plugins/power-manager-plugin/power-manager-button.c.orig	2024-06-09 15:35:08 UTC
 +++ panel-plugins/power-manager-plugin/power-manager-button.c
-@@ -510,6 +510,7 @@ power_manager_button_update_device_icon_and_details (P
-          * so we can disconnect it later */
-         battery_device->img = gtk_image_new_from_pixbuf (battery_device->pix);
+@@ -535,6 +535,7 @@ power_manager_button_update_device_icon_and_details (P
+     battery_device->img = gtk_image_new_from_surface (battery_device->surface);
+     g_object_ref (battery_device->img);
  
 +        g_object_ref (battery_device->img);
  G_GNUC_BEGIN_IGNORE_DEPRECATIONS
-         gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(battery_device->menu_item), battery_device->img);
+     gtk_image_menu_item_set_image(GTK_IMAGE_MENU_ITEM(battery_device->menu_item), battery_device->img);
  G_GNUC_END_IGNORE_DEPRECATIONS
